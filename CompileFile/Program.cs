182d4943cce6e1c1967f70e523316b210aa29096
JamesLin <James@smartcloud.com.tw>
2018-02-27 17:01:03 +0800

    因為明天放假，帶回家做

---
+++ CompileFile/Program.cs
@@ -0,0 +1,18 @@
+﻿using CSScriptLibrary;
+
+namespace CompileFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] par = { "System" };
+            //Compile
+            CSScript.CompileFile("Script.cs", @"C:\Users\j8351\Desktop\assembly", false, par);
+
+            //Loaded Assembly
+            AsmHelper asmHelper = new AsmHelper(@"C:\Users\j8351\Desktop\assembly", "test", false);
+            asmHelper.Invoke("*.Test");
+        }
+    }
+}
---
+++ CompileFile/Script.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CompileFile
+{
+    class Script
+    {
+        public static void Test()
+        {
+            Console.WriteLine("Test");
+        }
+    }
+}
