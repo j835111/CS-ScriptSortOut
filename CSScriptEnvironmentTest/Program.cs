73c51380db6947acfda7ada901a80c6ed66428da
JamesLin <James@smartcloud.com.tw>
2018-02-27 10:01:07 +0800

    Recreat CSScript and Rename to CSScriptEnvironmentTest

---
+++ CSScriptEnvironmentTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CSScriptEnvironmentTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CSScriptEvaluatorApi.HostApp.Test();
+            CSScriptEvaluatorExtensions.HostApp.Test();
+            CSScriptNativeApi.HostApp.Test();
+
+            Console.Write("Press any key to contiune...");
+            Console.Read();
+        }
+    }
+}
--- CSScriptEnvironmentTest/Properties/AssemblyInfo.cs
+++ CSScriptEnvironmentTest/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 組件的一般資訊是由下列的屬性集控制。
 // 變更這些屬性的值即可修改組件的相關
 // 資訊。
-[assembly: AssemblyTitle("CSScriptOriginal")]
+[assembly: AssemblyTitle("CSScriptEnvironmentTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CSScriptOriginal")]
+[assembly: AssemblyProduct("CSScriptEnvironmentTest")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 下列 GUID 為專案公開 (Expose) 至 COM 時所要使用的 typelib ID
-[assembly: Guid("0edb5825-ab97-4208-8001-937131288d34")]
+[assembly: Guid("2118c95e-dfa6-4a56-8019-d42728b6ec20")]
 
 // 組件的版本資訊由下列四個值所組成: 
 //
